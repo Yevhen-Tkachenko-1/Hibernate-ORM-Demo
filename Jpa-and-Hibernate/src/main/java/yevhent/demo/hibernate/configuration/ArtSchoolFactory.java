2df42b72dd7d4899c21f1908e3354368c2bfd0cd
Yevhen-Tkachenko <yevhen.tkachenko.1@gmail.com>
2024-11-13 22:23:43 +0200

    JPA implementation with Hibernate: - implement custom Factory to have ENV vars assigned in java

---
+++ Jpa-and-Hibernate/src/main/java/yevhent/demo/hibernate/configuration/ArtSchoolFactory.java
@@ -0,0 +1,18 @@
+package yevhent.demo.hibernate.configuration;
+
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ArtSchoolFactory {
+
+    public static EntityManagerFactory createEntityManagerFactory() {
+        Map<String, String> properties = new HashMap<>();
+        properties.put("jakarta.persistence.jdbc.user", System.getenv("POSTGRESQL_USER_NAME"));
+        properties.put("jakarta.persistence.jdbc.password", System.getenv("POSTGRESQL_USER_PASSWORD"));
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("art_school", properties);
+        return factory;
+    }
+}
