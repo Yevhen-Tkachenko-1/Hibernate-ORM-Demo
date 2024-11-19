aa0c4d32b6f6605cb70da533c4f02bb89bcea906
Yevhen-Tkachenko <yevhen.tkachenko.1@gmail.com>
2024-11-19 20:08:56 +0200

    JPA implementation with Hibernate: - reference and delete: "Art School" entity operations with Hibernate

---
+++ Jpa-and-Hibernate/src/main/java/yevhent/demo/hibernate/operation/ReferenceAndDeleteDemo.java
@@ -0,0 +1,25 @@
+package yevhent.demo.hibernate.operation;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import yevhent.demo.hibernate.configuration.ArtSchoolFactory;
+import yevhent.demo.hibernate.entity.ArtStudent;
+
+public class ReferenceAndDeleteDemo {
+    public static void main(String[] args) {
+
+        try (EntityManagerFactory entityManagerFactory = ArtSchoolFactory.createEntityManagerFactory();
+             EntityManager entityManager = entityManagerFactory.createEntityManager()) {
+            entityManager.getTransaction().begin();
+
+            // ArtStudent with ID = 1 must be persisted in DB before running ReferenceAndDeleteDemo
+            ArtStudent artStudent = entityManager.getReference(ArtStudent.class, 1);
+            // ArtStudent is just proxy object (with ID field only) implementing lazy loading
+            entityManager.remove(artStudent);
+            // Hibernate does not need to initialize the proxy or fetch additional data
+            entityManager.getTransaction().commit();
+            // Hibernate: delete from art_school.art_students where student_id=?
+            // call to DB
+        }
+    }
+}
