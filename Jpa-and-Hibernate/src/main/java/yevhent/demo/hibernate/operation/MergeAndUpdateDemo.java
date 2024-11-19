070cc40567d8f45a48fa7f800bab000b22c073ab
Yevhen-Tkachenko <yevhen.tkachenko.1@gmail.com>
2024-11-19 17:33:43 +0200

    JPA implementation with Hibernate: - merge and update "Art School" entity operations with Hibernate

---
+++ Jpa-and-Hibernate/src/main/java/yevhent/demo/hibernate/operation/MergeAndUpdateDemo.java
@@ -0,0 +1,25 @@
+package yevhent.demo.hibernate.operation;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import yevhent.demo.hibernate.configuration.ArtSchoolFactory;
+import yevhent.demo.hibernate.entity.ArtStudent;
+
+public class MergeAndUpdateDemo {
+    public static void main(String[] args) {
+
+        try (EntityManagerFactory entityManagerFactory = ArtSchoolFactory.createEntityManagerFactory();
+             EntityManager entityManager = entityManagerFactory.createEntityManager()) {
+            entityManager.getTransaction().begin();
+
+            // ArtStudent with ID = 1 must be persisted in DB before running MergeAndUpdateDemo
+            ArtStudent artStudent = new ArtStudent(1, "Merged and Updated John");
+            entityManager.merge(artStudent);
+            // Hibernate: select as1_0.student_id,as1_0.student_name from art_school.art_students as1_0 where as1_0.student_id=?
+            // call to DB
+            entityManager.getTransaction().commit();
+            // Hibernate: update art_school.art_students set student_name=? where student_id=?
+            // call to DB
+        }
+    }
+}
