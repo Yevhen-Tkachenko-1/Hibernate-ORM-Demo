00ecb76839f9a89488adee4e2d1ea9b5684eda87
Yevhen-Tkachenko <yevhen.tkachenko.1@gmail.com>
2024-11-20 19:19:53 +0200

    JPA implementation with Hibernate: - Relations: OneToMany insert demo

---
+++ Jpa-and-Hibernate/src/main/java/yevhent/demo/hibernate/relation/InsertOneToManyDemo.java
@@ -0,0 +1,57 @@
+package yevhent.demo.hibernate.relation;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import yevhent.demo.hibernate.configuration.ArtSchoolFactory;
+import yevhent.demo.hibernate.entity.ArtReview;
+import yevhent.demo.hibernate.entity.ArtTeacher;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+/**
+ * Unidirectional relation between Teacher and Class,
+ * where `art_classes` table has FK as reference to `art_teachers`
+ * and ArtClass object contains ArtTeacher object
+ */
+public class InsertOneToManyDemo {
+    public static void main(String[] args) {
+
+        try (EntityManagerFactory entityManagerFactory = ArtSchoolFactory.createEntityManagerFactory();
+             EntityManager entityManager = entityManagerFactory.createEntityManager()) {
+            entityManager.getTransaction().begin();
+
+            ArtTeacher artTeacher = new ArtTeacher(0, "John");
+            List<ArtReview> artReviews = IntStream.range(0, 10).boxed()
+                    .map(i -> new ArtReview(0, "Review number " + i + " by Teacher " + artTeacher.getName(), i * 10, artTeacher))
+                    .collect(Collectors.toList());
+            artTeacher.setArtReviews(artReviews);
+
+            entityManager.persist(artTeacher);
+            // Hibernate: insert into art_school.art_teachers (teacher_name) values (?) returning teacher_id
+            // Hibernate: insert into art_school.art_reviews (teacher_id,review_comment,rating) values (?,?,?) returning review_id
+            // Hibernate: insert into art_school.art_reviews (teacher_id,review_comment,rating) values (?,?,?) returning review_id
+            // Hibernate: insert into art_school.art_reviews (teacher_id,review_comment,rating) values (?,?,?) returning review_id
+            // Hibernate: insert into art_school.art_reviews (teacher_id,review_comment,rating) values (?,?,?) returning review_id
+            // Hibernate: insert into art_school.art_reviews (teacher_id,review_comment,rating) values (?,?,?) returning review_id
+            // Hibernate: insert into art_school.art_reviews (teacher_id,review_comment,rating) values (?,?,?) returning review_id
+            // Hibernate: insert into art_school.art_reviews (teacher_id,review_comment,rating) values (?,?,?) returning review_id
+            // Hibernate: insert into art_school.art_reviews (teacher_id,review_comment,rating) values (?,?,?) returning review_id
+            // Hibernate: insert into art_school.art_reviews (teacher_id,review_comment,rating) values (?,?,?) returning review_id
+            // Hibernate: insert into art_school.art_reviews (teacher_id,review_comment,rating) values (?,?,?) returning review_id
+            entityManager.getTransaction().commit();
+            // insert ArtTeacher to DB
+            // insert ArtReview to DB
+            // insert ArtReview to DB
+            // insert ArtReview to DB
+            // insert ArtReview to DB
+            // insert ArtReview to DB
+            // insert ArtReview to DB
+            // insert ArtReview to DB
+            // insert ArtReview to DB
+            // insert ArtReview to DB
+            // insert ArtReview to DB
+        }
+    }
+}
