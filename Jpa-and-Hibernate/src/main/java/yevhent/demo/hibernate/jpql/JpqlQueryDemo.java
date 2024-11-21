35918710e7570a8e096e57c6b70fe21f2dedf4c8
Yevhen-Tkachenko <yevhen.tkachenko.1@gmail.com>
2024-11-21 18:21:43 +0200

    JPA implementation with Hibernate: - JPQL queries: java demo

--- Jpa-and-Hibernate/src/main/java/yevhent/demo/hibernate/jpql/JpqlQueryDemo.java
+++ Jpa-and-Hibernate/src/main/java/yevhent/demo/hibernate/jpql/JpqlQueryDemo.java
@@ -4,6 +4,7 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.TypedQuery;
 import yevhent.demo.hibernate.configuration.ArtSchoolFactory;
+import yevhent.demo.hibernate.entity.ArtClass;
 import yevhent.demo.hibernate.entity.ArtStudent;
 
 import java.util.List;
@@ -12,11 +13,14 @@ public class JpqlQueryDemo {
 
     public static void main(String[] args) {
 
-        System.out.println("All ArtStudents: " + findAllStudents());
-        // All ArtStudents: [ArtStudent(id=1, name=John), ArtStudent(id=2, name=Alice), ArtStudent(id=3, name=Bob), ArtStudent(id=4, name=Charlie), ArtStudent(id=5, name=Diana), ArtStudent(id=6, name=Eve), ArtStudent(id=7, name=Frank)]
+        findAllStudents();
+        findStudentClasses();
+        findTeacherAverageReviewRating();
+        findTeachersAverageReviewRatings();
+        findTeachersAverageReviewRatingsGreaterThan40();
     }
 
-    public static List<ArtStudent> findAllStudents() {
+    public static void findAllStudents() {
         try (EntityManagerFactory entityManagerFactory = ArtSchoolFactory.createEntityManagerFactory();
              EntityManager entityManager = entityManagerFactory.createEntityManager()) {
 
@@ -25,7 +29,86 @@ public class JpqlQueryDemo {
             List<ArtStudent> artStudents = q1.getResultList();
             // Hibernate: select as1_0.student_id,as1_0.student_name from art_school.art_students as1_0
             // Select query to DB
-            return artStudents;
+            System.out.println("All Students: " + artStudents);
+            // All Students: [ArtStudent(id=1, name=John), ArtStudent(id=2, name=Alice), ArtStudent(id=3, name=Bob), ArtStudent(id=4, name=Charlie), ArtStudent(id=5, name=Diana), ArtStudent(id=6, name=Eve), ArtStudent(id=7, name=Frank)]
+        }
+    }
+
+    public static void findStudentClasses() {
+        try (EntityManagerFactory entityManagerFactory = ArtSchoolFactory.createEntityManagerFactory();
+             EntityManager entityManager = entityManagerFactory.createEntityManager()) {
+
+            String s1 = "SELECT s.artClasses FROM ArtStudent s WHERE s.name = 'John'";
+            TypedQuery<ArtClass> q1 = entityManager.createQuery(s1, ArtClass.class);
+            List<ArtClass> artClasses = q1.getResultList();
+            // Hibernate: select ac1_1.class_id,ac1_1.teacher_id,ac1_1.class_name,ac1_1.week_day
+            //            from art_school.art_students as1_0
+            //            join art_school.students_classes_mapping ac1_0 on as1_0.student_id=ac1_0.student_id
+            //            join art_school.art_classes ac1_1 on ac1_1.class_id=ac1_0.class_id
+            //            where as1_0.student_name='John'
+            // Select query to DB
+            System.out.println("Student John attends " + artClasses);
+            // Student John attends [ArtClass(id=5, name=Painting, weekDay=Monday), ArtClass(id=6, name=Music, weekDay=Saturday)]
+        }
+    }
+
+    public static void findTeacherAverageReviewRating() {
+        try (EntityManagerFactory entityManagerFactory = ArtSchoolFactory.createEntityManagerFactory();
+             EntityManager entityManager = entityManagerFactory.createEntityManager()) {
+
+            String s1 = "SELECT AVG(r.rating) FROM ArtReview r WHERE r.artTeacher.name = 'John'";
+            TypedQuery<Double> q1 = entityManager.createQuery(s1, Double.class);
+            Double averageRating = q1.getSingleResult();
+            // Hibernate: select avg(ar1_0.rating)
+            //            from art_school.art_reviews ar1_0
+            //            join art_school.art_teachers at1_0 on at1_0.teacher_id=ar1_0.teacher_id
+            //            where at1_0.teacher_name='John'
+            // Select query to DB
+            System.out.println("Teacher John rated in average as " + averageRating);
+            // Teacher John rated in average as 45.0
+        }
+    }
+
+    public static void findTeachersAverageReviewRatings() {
+        try (EntityManagerFactory entityManagerFactory = ArtSchoolFactory.createEntityManagerFactory();
+             EntityManager entityManager = entityManagerFactory.createEntityManager()) {
+
+            String s1 = "SELECT t.name, AVG(r.rating) FROM ArtReview r JOIN r.artTeacher t GROUP BY t.name";
+            TypedQuery<Object[]> q1 = entityManager.createQuery(s1, Object[].class);
+            List<Object[]> averageRatings = q1.getResultList();
+            // Hibernate: select at1_0.teacher_name,avg(ar1_0.rating)
+            //            from art_school.art_reviews ar1_0
+            //            join art_school.art_teachers at1_0 on at1_0.teacher_id=ar1_0.teacher_id
+            //            group by at1_0.teacher_name
+            // Select query to DB
+            for (Object[] averageRating : averageRatings) {
+                System.out.printf("Teacher %s rated in average as %s\n", averageRating[0], averageRating[1]);
+            }
+            // Teacher Mike rated in average as 45.0
+            // Teacher Joe rated in average as 45.0
+            // Teacher John rated in average as 45.0
+        }
+    }
+
+    public static void findTeachersAverageReviewRatingsGreaterThan40() {
+        try (EntityManagerFactory entityManagerFactory = ArtSchoolFactory.createEntityManagerFactory();
+             EntityManager entityManager = entityManagerFactory.createEntityManager()) {
+
+            String s1 = "SELECT t.name, AVG(r.rating) FROM ArtReview r JOIN r.artTeacher t GROUP BY t.name HAVING AVG(r.rating) > 40 ORDER BY AVG(r.rating) DESC";
+            TypedQuery<Object[]> q1 = entityManager.createQuery(s1, Object[].class);
+            List<Object[]> averageRatings = q1.getResultList();
+            // Hibernate: select at1_0.teacher_name,avg(ar1_0.rating)
+            //            from art_school.art_reviews ar1_0
+            //            join art_school.art_teachers at1_0 on at1_0.teacher_id=ar1_0.teacher_id
+            //            group by at1_0.teacher_name
+            //            having avg(ar1_0.rating)>40
+            //            order by avg(ar1_0.rating) desc
+            // Select query to DB
+            for (Object[] averageRating : averageRatings) {
+                System.out.printf("Teacher %s rated in average as %s\n", averageRating[0], averageRating[1]);
+            }
+            // Teacher Mike rated in average as 45.0
+            // Teacher Joe rated in average as 45.0
         }
     }
 }
