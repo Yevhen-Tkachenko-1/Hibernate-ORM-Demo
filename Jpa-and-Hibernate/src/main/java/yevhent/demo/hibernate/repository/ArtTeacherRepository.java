6bc80b94c53507ec8d9ef05959364cca00478fc2
Yevhen-Tkachenko <yevhen.tkachenko.1@gmail.com>
2024-11-28 20:10:15 +0200

    JPA implementation with Hibernate: - Repository: define Teacher interface

--- Jpa-and-Hibernate/src/main/java/yevhent/demo/hibernate/repository/ArtTeacherRepository.java
+++ Jpa-and-Hibernate/src/main/java/yevhent/demo/hibernate/repository/ArtTeacherRepository.java
@@ -3,15 +3,17 @@ package yevhent.demo.hibernate.repository;
 import jakarta.persistence.EntityManager;
 import yevhent.demo.hibernate.entity.ArtTeacher;
 
+import java.util.Optional;
+
 public interface ArtTeacherRepository extends ArtCrudRepository<ArtTeacher> {
 
     /**
      * For given Teacher calculates average rating of all its Reviews
      * @param entityManager data source and transaction holder
      * @param id Teacher ID (from 1 to Integer.MAX_VALUE)
-     * @return average Review rating for given Teacher
+     * @return average Review rating for given Teacher if given Teacher is present and has at least one Review
      */
-    Double getAverageRating(EntityManager entityManager, int teacherId);
+    Optional<Double> getAverageRating(EntityManager entityManager, int teacherId);
 
     /**
      * For given Teacher deletes any rating that is lower than given value exclusively
