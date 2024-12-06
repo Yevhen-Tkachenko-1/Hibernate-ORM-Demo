04ca41c8a572683908969e2ac5a0a18ad423191d
Yevhen-Tkachenko <yevhen.tkachenko.1@gmail.com>
2024-12-06 15:13:27 +0200

    JPA implementation with Hibernate: - CrudQuery: finish NativeQueryDemo

--- Jpa-and-Hibernate/src/main/java/yevhent/demo/hibernate/query/NativeQueryDemo.java
+++ Jpa-and-Hibernate/src/main/java/yevhent/demo/hibernate/query/NativeQueryDemo.java
@@ -7,6 +7,8 @@ import jakarta.persistence.Tuple;
 import yevhent.demo.hibernate.configuration.ArtSchoolFactory;
 import yevhent.demo.hibernate.entity.ArtReview;
 import yevhent.demo.hibernate.entity.ArtTeacher;
+import yevhent.demo.hibernate.query.crud.CrudQuery;
+import yevhent.demo.hibernate.query.crud.CrudQueryUser;
 
 import java.util.List;
 import java.util.Map;
@@ -17,9 +19,11 @@ public class NativeQueryDemo implements CrudQuery {
     public static void main(String[] args) {
 
         CrudQuery crudQuery = new NativeQueryDemo();
-        Map<ArtTeacher, List<ArtReview>> entities = CrudQueryUser.create(crudQuery);
 
-        CrudQueryUser.read(crudQuery);
+        Map<ArtTeacher, List<ArtReview>> entities = CrudQueryUser.create(crudQuery);
+        CrudQueryUser.read(crudQuery, entities);
+        CrudQueryUser.update(crudQuery, entities);
+        CrudQueryUser.delete(crudQuery, entities);
     }
 
     @Override
@@ -27,13 +31,15 @@ public class NativeQueryDemo implements CrudQuery {
         try (EntityManagerFactory entityManagerFactory = ArtSchoolFactory.createEntityManagerFactory();
              EntityManager entityManager = entityManagerFactory.createEntityManager()) {
 
-            String sql = "INSERT INTO art_school.art_teachers (teacher_name)" +
-                    " VALUES (:name)" +
-                    " RETURNING *";
+            String sql = """
+                    INSERT INTO art_school.art_teachers (teacher_name)
+                    VALUES (:name)
+                    RETURNING *;
+                    """;
             Query query = entityManager.createNativeQuery(sql, ArtTeacher.class);
             query.setParameter("name", name);
             ArtTeacher teacher = (ArtTeacher) query.getSingleResult();
-            System.out.println("Created " + teacher);
+            System.out.printf("Created %s.\n", teacher);
             return teacher;
         }
     }
@@ -44,15 +50,17 @@ public class NativeQueryDemo implements CrudQuery {
              EntityManager entityManager = entityManagerFactory.createEntityManager()) {
 
             List<ArtReview> reviews = ratings.stream().map(rating -> {
-                String sql = "INSERT INTO art_school.art_reviews (review_comment, rating, teacher_id)" +
-                        " VALUES (:comment, :rating, :teacherId)" +
-                        " RETURNING *"; // review_id, review_comment, rating and ArtTeacher entity
+                String sql = """
+                        INSERT INTO art_school.art_reviews (review_comment, rating, teacher_id)
+                        VALUES (:comment, :rating, :teacherId)
+                        RETURNING *;
+                        """;
                 Query query = entityManager.createNativeQuery(sql, ArtReview.class);
-                query.setParameter("comment", "Commented by Teacher[" + teacherId + "] and rated as " + rating);
+                query.setParameter("comment", String.format("Commented by Teacher(%d) and rated as %d.", teacherId, rating));
                 query.setParameter("rating", rating);
                 query.setParameter("teacherId", teacherId);
                 ArtReview review = (ArtReview) query.getSingleResult();
-                System.out.println("Created " + review + " for "+ review.getArtTeacher());
+                System.out.printf("Created %s related to %s.\n", review, review.getArtTeacher());
                 return review;
             }).collect(Collectors.toList());
             return reviews;
@@ -60,37 +68,81 @@ public class NativeQueryDemo implements CrudQuery {
     }
 
     @Override
-    public Map<String, Long> findTeachersWithReviewNumber(int minNumberOfReviews) {
+    public Map<String, Long> findTeachersWithReviewNumberMore(int teacherIdFrom, int teacherIdTo, int minNumberOfReviews) {
         try (EntityManagerFactory entityManagerFactory = ArtSchoolFactory.createEntityManagerFactory();
              EntityManager entityManager = entityManagerFactory.createEntityManager()) {
 
-            String sql = "SELECT t.teacher_name AS name, COUNT(*) AS number ";
-            sql += " FROM art_school.art_teachers AS t";
-            sql += " JOIN art_school.art_reviews AS r ON t.teacher_id = r.teacher_id";
-            sql += " GROUP BY t.teacher_id, t.teacher_name";
-            sql += " HAVING COUNT(*) > :minNumber";
+            String sql = """
+                    SELECT t.teacher_id AS id, t.teacher_name AS name, COUNT(*) AS number
+                    FROM art_school.art_teachers AS t
+                    JOIN art_school.art_reviews AS r ON t.teacher_id = r.teacher_id
+                    WHERE t.teacher_id BETWEEN :teacherIdFrom AND :teacherIdTo
+                    GROUP BY t.teacher_id, t.teacher_name
+                    HAVING COUNT(*) > :minNumber
+                    """;
             Query query = entityManager.createNativeQuery(sql, Tuple.class);
+            query.setParameter("teacherIdFrom", teacherIdFrom);
+            query.setParameter("teacherIdTo", teacherIdTo);
             query.setParameter("minNumber", minNumberOfReviews);
             List<Tuple> rows = query.getResultList();
             Map<String, Long> teacherReviewNumbers = rows.stream()
-                    .peek(row -> System.out.println("Found Teacher " + row.get("name", String.class) + " with " + row.get("number", Long.class) + " reviews"))
-                    .collect(Collectors.toMap(row -> row.get("name", String.class), row -> row.get("number", Long.class)));
+                    .peek(row -> System.out.printf("Found Teacher(%d, \"%s\") with %s reviews.\n",
+                            row.get("id", Integer.class), row.get("name", String.class), row.get("number", Long.class)))
+                    .collect(Collectors.toMap(
+                            row -> String.format("Teacher(%d, \"%s\")", row.get("id", Integer.class), row.get("name", String.class)),
+                            row -> row.get("number", Long.class)));
             return teacherReviewNumbers;
         }
     }
 
     @Override
-    public Map<String, Double> findAverageReviewsRatings(int minAverageRating) {
-        return null;
+    public Map<String, Integer> roundReviewRatings(int teacherId) {
+        try (EntityManagerFactory entityManagerFactory = ArtSchoolFactory.createEntityManagerFactory();
+             EntityManager entityManager = entityManagerFactory.createEntityManager()) {
+
+            String sql = """
+                    UPDATE art_school.art_reviews
+                    SET rating = ROUND(rating / 10) * 10
+                    WHERE teacher_id = :teacherId
+                    RETURNING review_id AS id, review_comment AS comment, rating AS new_rating;
+                    """;
+            Query query = entityManager.createNativeQuery(sql, Tuple.class);
+            query.setParameter("teacherId", teacherId);
+            List<Tuple> rows = query.getResultList();
+            Map<String, Integer> reviews = rows.stream()
+                    .peek(row -> System.out.printf("Updated Rating in Review(%d, \"%s\") to %d.\n",
+                            row.get("id", Integer.class), row.get("comment", String.class), row.get("new_rating", Integer.class)))
+                    .collect(Collectors.toMap(
+                            row -> String.format("Review(%d, \"%s\")", row.get("id", Integer.class), row.get("comment", String.class)),
+                            row -> row.get("new_rating", Integer.class)));
+            return reviews;
+        }
     }
 
     @Override
-    public Map<String, List<Integer>> roundReviewRatings(int teacherId) {
-        return null;
-    }
+    public Map<String, Integer> deleteReviewsWithRatingLower(int teacherId, int minRating) {
+        try (EntityManagerFactory entityManagerFactory = ArtSchoolFactory.createEntityManagerFactory();
+             EntityManager entityManager = entityManagerFactory.createEntityManager()) {
 
-    @Override
-    public int deleteReviewsLower(int teacherId, int rating) {
-        return 0;
+            String sql = """
+                    WITH deleted_reviews AS (
+                        DELETE FROM art_school.art_reviews
+                        WHERE teacher_id = :teacherId AND rating < :minRating
+                        RETURNING review_id AS id, review_comment AS comment, rating AS lower_rating
+                    )
+                    SELECT id, comment, lower_rating FROM deleted_reviews;
+                    """;
+            Query query = entityManager.createNativeQuery(sql, Tuple.class);
+            query.setParameter("teacherId", teacherId);
+            query.setParameter("minRating", minRating);
+            List<Tuple> rows = query.getResultList();
+            Map<String, Integer> reviews = rows.stream()
+                    .peek(row -> System.out.printf("Deleted Review(%d, \"%s\", %d).\n",
+                            row.get("id", Integer.class), row.get("comment", String.class), row.get("lower_rating", Integer.class)))
+                    .collect(Collectors.toMap(
+                            row -> String.format("Review(%d, \"%s\")", row.get("id", Integer.class), row.get("comment", String.class)),
+                            row -> row.get("lower_rating", Integer.class)));
+            return reviews;
+        }
     }
 }
