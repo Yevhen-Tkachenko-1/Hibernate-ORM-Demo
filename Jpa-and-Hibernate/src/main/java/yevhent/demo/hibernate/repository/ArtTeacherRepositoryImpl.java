70a7ae870151cf4ce53f6dd90873bf5dc2150d14
Yevhen-Tkachenko <yevhen.tkachenko.1@gmail.com>
2024-12-10 18:21:52 +0200

    JPA implementation with Hibernate: - Repository: Teacher Repo implementation

--- Jpa-and-Hibernate/src/main/java/yevhent/demo/hibernate/repository/ArtTeacherRepositoryImpl.java
+++ Jpa-and-Hibernate/src/main/java/yevhent/demo/hibernate/repository/ArtTeacherRepositoryImpl.java
@@ -1,53 +1,26 @@
 package yevhent.demo.hibernate.repository;
 
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
 import yevhent.demo.hibernate.entity.ArtTeacher;
 
-import java.util.Optional;
+import java.util.List;
 
-public class ArtTeacherRepositoryImpl implements ArtTeacherRepository {
+public class ArtTeacherRepositoryImpl extends BaseCrudRepository<ArtTeacher> implements ArtTeacherRepository {
 
-    @Override
-    public ArtTeacher create(EntityManager entityManager, ArtTeacher entity) {
-        entityManager.persist(entity);
-        return entity;
+    public ArtTeacherRepositoryImpl() {
+        super(ArtTeacher.class);
     }
 
     @Override
-    public Optional<ArtTeacher> read(EntityManager entityManager, int id) {
-        return Optional.ofNullable(entityManager.find(ArtTeacher.class, id));
+    public List<ArtTeacher> findEagerTeachersByIds(EntityManager entityManager, List<Integer> teacherIds) {
+        return entityManager.createQuery("""
+                                SELECT t FROM ArtTeacher t
+                                LEFT JOIN FETCH t.artReviews
+                                WHERE t.id IN :teacherIds
+                                """, ArtTeacher.class)
+                .setParameter("teacherIds", teacherIds)
+                .getResultList();
     }
 
-    @Override
-    public boolean update(EntityManager entityManager, ArtTeacher entity) {
-        ArtTeacher artTeacher = entityManager.find(ArtTeacher.class, entity.getId());
-        if (artTeacher == null) {
-            return false;
-        }
-        if (artTeacher.equals(entity)) {
-            return false;
-        }
-        entityManager.merge(entity);
-        return true;
-    }
-
-    @Override
-    public boolean delete(EntityManager entityManager, int id) {
-        ArtTeacher artTeacher = entityManager.find(ArtTeacher.class, id);
-        if (artTeacher == null) {
-            return false;
-        }
-        entityManager.remove(artTeacher);
-        return true;
-    }
-
-    @Override
-    public Double getAverageRating(EntityManager entityManager, int teacherId) {
-        return null;
-    }
-
-    @Override
-    public boolean deleteRatingsBelow(EntityManager entityManager, int teacherId, int minAllowedRating) {
-        return false;
-    }
 }
