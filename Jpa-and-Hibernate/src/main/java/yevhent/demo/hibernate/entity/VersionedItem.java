a3013e263373f4a1a857db0e835e347f583c63a6
Yevhen-Tkachenko <yevhen.tkachenko.1@gmail.com>
2024-12-16 20:13:02 +0200

    JPA implementation with Hibernate: - create new Entity to use versioning

---
+++ Jpa-and-Hibernate/src/main/java/yevhent/demo/hibernate/entity/VersionedItem.java
@@ -0,0 +1,32 @@
+package yevhent.demo.hibernate.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(schema = "public", name = "versioned_items")
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@EqualsAndHashCode
+@ToString
+public class VersionedItem {
+
+    public VersionedItem(String name) {
+        this.name = name;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "item_id")
+    private int id;
+
+    @Column(name = "item_name")
+    private String name;
+
+    @Version
+    @Column(name = "version")
+    private int version;
+
+}
