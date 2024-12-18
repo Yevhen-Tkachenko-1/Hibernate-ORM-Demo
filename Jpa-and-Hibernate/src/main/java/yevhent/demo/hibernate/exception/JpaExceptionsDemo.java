af87ed054fd9dc0b63063a1bc7cc6dc66a7aaaff
Yevhen-Tkachenko <yevhen.tkachenko.1@gmail.com>
2024-12-18 20:24:55 +0200

    JPA implementation with Hibernate: - Exceptions: keep only JPA ones in this class

--- Jpa-and-Hibernate/src/main/java/yevhent/demo/hibernate/exception/JpaExceptionsDemo.java
+++ Jpa-and-Hibernate/src/main/java/yevhent/demo/hibernate/exception/JpaExceptionsDemo.java
@@ -1,15 +1,12 @@
 package yevhent.demo.hibernate.exception;
 
 import jakarta.persistence.*;
-import org.hibernate.LazyInitializationException;
+import lombok.SneakyThrows;
 import yevhent.demo.hibernate.configuration.ArtSchoolFactory;
-import yevhent.demo.hibernate.entity.ArtReview;
 import yevhent.demo.hibernate.entity.ArtTeacher;
-import yevhent.demo.hibernate.entity.SelfIdentifiable;
-import yevhent.demo.hibernate.entity.VersionedItem;
 
-import java.util.List;
-import java.util.stream.IntStream;
+import java.util.HashMap;
+import java.util.Map;
 
 public class JpaExceptionsDemo {
 
@@ -17,121 +14,106 @@ public class JpaExceptionsDemo {
 
         Thread.setDefaultUncaughtExceptionHandler((t, e) -> {
             System.out.println("===== Uncaught exception in thread \"" + t.getName() + "\":");
-            System.out.println(e);
+            e.printStackTrace();
         });
-        System.out.println("=====throwTransactionRequiredException===========================================");
-        throwTransactionRequiredException();
-        System.out.println("=====throwEntityExistsException============================================");
-        throwEntityExistsException();
-        System.out.println("=====throwLazyInitializationException============================================");
-        throwLazyInitializationException();
-        System.out.println("=====throwStaleStateException====================================================");
-        throwStaleStateException();
-        System.out.println("=====END====================================================");
+        try {
+            System.out.println("=====throwEntityNotFoundException============================================");
+            throwEntityNotFoundException();
+            System.out.println("=====throwTransactionRequiredException====================================================");
+            throwTransactionRequiredException();
+            System.out.println("=====throwLockTimeoutException====================================================");
+            throwLockTimeoutException();
+            System.out.println("=====END====================================================");
+        } catch (Exception e) {
+            System.out.println("===== Caught exception in main thread:");
+            e.printStackTrace();
+        }
+    }
+
+    static void throwEntityNotFoundException() {
+
+        int teacherId = ExceptionRepository.saveNewTeacherAndReviewsToDB();
+        ExceptionRepository.deleteTeacherAndReviewsFromDB(teacherId);
+
+        try (EntityManagerFactory entityManagerFactory = ArtSchoolFactory.createEntityManagerFactory();
+             EntityManager entityManager = entityManagerFactory.createEntityManager()) {
+            ArtTeacher teacherProxy = entityManager.getReference(ArtTeacher.class, teacherId);
+            System.out.println(teacherProxy.getName()); // throws EntityNotFoundException
+        } catch (EntityNotFoundException e) {
+            System.out.println(e);
+            // jakarta.persistence.EntityNotFoundException:
+            // Unable to find yevhent.demo.hibernate.entity.ArtTeacher with id 320
+        }
+    }
+
+    @SneakyThrows(InterruptedException.class)
+    static void throwLockTimeoutException() {
+        Map<String, Object> props = new HashMap();
+        props.put("javax.persistence.lock.timeout", 10_000L);
+        props.put("jakarta.persistence.lock.timeout", 10_000L);
+        props.put("hibernate.transaction.timeout", 10);
+
+        int id = ExceptionRepository.saveNewTeacherAndReviewsToDB();
+
+        try (EntityManagerFactory entityManagerFactory = ArtSchoolFactory.createEntityManagerFactory();
+             EntityManager entityManager1 = entityManagerFactory.createEntityManager();
+             EntityManager entityManager2 = entityManagerFactory.createEntityManager()) {
+            entityManager2.setProperty("javax.persistence.lock.timeout", 10_000L);
+            entityManager2.setProperty("jakarta.persistence.lock.timeout", 10_000L);
+            Thread transaction1 = new Thread(() -> {
+                System.out.println("Transaction 1 started.");
+                entityManager1.getTransaction().begin();
+                ArtTeacher teacher1 = entityManager1.find(ArtTeacher.class, id, LockModeType.PESSIMISTIC_WRITE);
+                System.out.println("Transaction 1 acquired lock on: " + teacher1);
+                System.out.println("Transaction 1 sleeps ...");
+                try {
+                    // Keep Transaction 1 open to block the lock
+                    Thread.sleep(70000); // Simulate a long transaction
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println("Transaction 1 commits ...");
+                entityManager1.getTransaction().commit();
+                System.out.println("Transaction 1 ended.");
+            });
+            Thread transaction2 = new Thread(() -> {
+                System.out.println("Transaction 2 started.");
+                try {
+                    entityManager2.getTransaction().begin();
+                    System.out.println("Transaction 2 attempting to acquire lock...");
+                    ArtTeacher teacher2 = entityManager2.find(ArtTeacher.class, id, LockModeType.PESSIMISTIC_WRITE, props);
+                    System.out.println("Transaction 2 acquired lock on: " + teacher2);
+                    entityManager2.getTransaction().commit();
+                    System.out.println("Transaction 2 ended.");
+                } catch (Exception e) {
+                    System.out.println(e);
+                    System.out.println(e.getCause());
+                    System.out.println(e.getCause().getCause());
+                } finally {
+                    if (entityManager2.getTransaction().isActive()) {
+                        System.out.println("isActive=true, rollback");
+                        entityManager2.getTransaction().rollback();
+                    }
+                }
+            });
+            transaction1.start();
+            transaction2.start();
+            transaction1.join();
+            transaction2.join();
+            System.out.println("throwLockTimeoutException end");
+        }
     }
 
     static void throwTransactionRequiredException() {
         try (EntityManagerFactory entityManagerFactory = ArtSchoolFactory.createEntityManagerFactory();
              EntityManager entityManager = entityManagerFactory.createEntityManager()) {
 
-            ArtTeacher teacher = new ArtTeacher("John Doe");
+            ArtTeacher teacher = new ArtTeacher("Prof. Davis");
             entityManager.persist(teacher);
             entityManager.flush(); // throws TransactionRequiredException
         } catch (TransactionRequiredException e) {
             System.out.println(e);
             // jakarta.persistence.TransactionRequiredException: no transaction is in progress
-        }
-    }
-
-    static void throwEntityExistsException() {
-
-        try (EntityManagerFactory entityManagerFactory = ArtSchoolFactory.createEntityManagerFactory();
-             EntityManager entityManager = entityManagerFactory.createEntityManager()) {
-            entityManager.getTransaction().begin();
-
-            entityManager.persist(new SelfIdentifiable(1));
-            entityManager.persist(new SelfIdentifiable(1)); // throws EntityExistsException
-
-        } catch (EntityExistsException e) {
-            System.out.println(e);
-            // jakarta.persistence.EntityExistsException:
-            // A different object with the same identifier value was already associated with the session
-            System.out.println(e.getCause());
-            // org.hibernate.NonUniqueObjectException:
-            // A different object with the same identifier value was already associated with the session
-        }
-    }
-
-    static void throwLazyInitializationException() {
-
-        int teacherId = createNewTeacherInDB();
-        ArtTeacher teacher;
-
-        try (EntityManagerFactory entityManagerFactory = ArtSchoolFactory.createEntityManagerFactory();
-             EntityManager entityManager = entityManagerFactory.createEntityManager()) {
-
-            teacher = entityManager.find(ArtTeacher.class, teacherId);
-        } // EntityManager is closed by try-with-resources (reason of LazyInitializationException)
-        try {
-            teacher.getArtReviews().size(); // throws LazyInitializationException
-        } catch (LazyInitializationException e) {
-            System.out.println(e);
-            // org.hibernate.LazyInitializationException:
-            // failed to lazily initialize a collection of role:
-            // yevhent.demo.hibernate.entity.ArtTeacher.artReviews:
-            // could not initialize proxy - no Session
-        }
-    }
-
-    static void throwStaleStateException() {
-
-        try (EntityManagerFactory entityManagerFactory = ArtSchoolFactory.createEntityManagerFactory();
-             EntityManager entityManager1 = entityManagerFactory.createEntityManager();
-             EntityManager entityManager2 = entityManagerFactory.createEntityManager()) {
-
-            entityManager1.getTransaction().begin();
-
-            VersionedItem item = new VersionedItem("Item0000");
-            entityManager1.persist(item);
-            entityManager1.getTransaction().commit();
-
-            VersionedItem item11 = entityManager1.find(VersionedItem.class, item.getId());
-
-            entityManager2.getTransaction().begin();
-            VersionedItem item12 = entityManager2.find(VersionedItem.class, item.getId());
-            item12.setName("Item2222(set by Transaction2)");
-            entityManager2.getTransaction().commit();
-
-            entityManager1.getTransaction().begin();
-            item11.setName("Item1111(set by Transaction1)");
-            entityManager1.getTransaction().commit(); // Throws RollbackException
-        } catch (RollbackException e) {
-            System.out.println(e);
-            // jakarta.persistence.RollbackException:
-            // Error while committing the transaction
-            System.out.println(e.getCause());
-            // jakarta.persistence.OptimisticLockException:
-            // Row was updated or deleted by another transaction (or unsaved-value mapping was incorrect):
-            // [yevhent.demo.hibernate.entity.VersionedItem#33]
-            System.out.println(e.getCause().getCause());
-            // org.hibernate.StaleObjectStateException:
-            // Row was updated or deleted by another transaction (or unsaved-value mapping was incorrect):
-            // [yevhent.demo.hibernate.entity.VersionedItem#33]
-        }
-    }
-
-
-    static int createNewTeacherInDB() {
-        try (EntityManagerFactory entityManagerFactory = ArtSchoolFactory.createEntityManagerFactory();
-             EntityManager entityManager = entityManagerFactory.createEntityManager()) {
-            entityManager.getTransaction().begin();
-
-            ArtTeacher teacher = new ArtTeacher("Prof. Davis");
-            List<ArtReview> artReviews = IntStream.range(0, 5).boxed().map(i -> new ArtReview("Review number " + i, i * 10, teacher)).toList();
-            teacher.setArtReviews(artReviews);
-            entityManager.persist(teacher);
-            entityManager.getTransaction().commit();
-            return teacher.getId();
         }
     }
 }
